1dcb28d63cf630fecca1daa99aacd3674001c003
윤성훈 <79916054+seonghun2@users.noreply.github.com>
2023-12-05 22:48:55 +0900

    Title: 겹치지 않게 선분 고르기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231205/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,31 @@
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+selectedLine = []
+selectedLine.sort(key = lambda x : x[0])
+ans = 0
+
+def makeLine(cur):
+    global ans
+    if cur == n :
+        if isOverlap(selectedLine):
+            ans = max(ans, len(selectedLine))
+        return
+    
+    selectedLine.append(arr[cur])
+    makeLine(cur + 1)
+    selectedLine.pop()
+
+    makeLine(cur + 1)
+
+def isOverlap(lines):
+    for i in range(len(lines)-1):
+        if lines[i][1] >= lines[i+1][0]:
+            return False
+    return True
+
+makeLine(0)
+print(ans)
