1605468378859253a63e70e6553ed98a44169240
윤성훈 <79916054+seonghun2@users.noreply.github.com>
2023-12-05 23:53:49 +0900

    Title: 겹치지 않게 선분 고르기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231205/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 231205/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -1,19 +1,21 @@
 n = int(input())
-# arr = [[491, 96],[500, 967],[311, 878],[53, 720],[634, 728],[380, 570],[55, 682]]
+# n = 7
+# arr = [[491, 696],[500, 967],[311, 878],[53, 720],[634, 728],[380, 570],[55, 682]]
 arr = []
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
 selectedLine = []
-selectedLine.sort(key = lambda x : x[0])
+
 ans = 0
 
 def makeLine(cur):
     global ans
 
     if cur == n :
-        # print(selectedLine)
+        selectedLine.sort(key = lambda x : x[0])
         if isOverlap(selectedLine):
+            # print(selectedLine)
             ans = max(ans, len(selectedLine))
         return
     
