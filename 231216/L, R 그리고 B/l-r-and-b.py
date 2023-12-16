ace474a6b941d63b3aa787d38dac55820ac76f19
윤성훈 <79916054+seonghun2@users.noreply.github.com>
2023-12-16 17:14:14 +0900

    Title: L, R 그리고 B, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 231216/L, R 그리고 B/l-r-and-b.py
+++ 231216/L, R 그리고 B/l-r-and-b.py
@@ -4,6 +4,7 @@ for _ in range(10):
 
 L = ()
 B = ()
+R = ()
 
 for i in range(10):
     for j in range(10):
@@ -11,6 +12,13 @@ for i in range(10):
             B = (i,j)
         if arr[i][j] == 'L':
             L = (i,j)
+        if arr[i][j] == 'R':
+            R = (i,j)
 
-ans = abs(L[0] - B[0]) + abs(L[1] - B[1]) - 1
+if L[0] == R[0] and R[0] == B[0] and R[1] > min(L[1],B[1]) and R[1] < max(L[1],B[1]):
+    ans = abs(L[0] - B[0]) + abs(L[1] - B[1]) + 1
+elif L[1] == R[1] and R[1] == B[1] and R[0] > min(L[0],B[0]) and R[0] < max(L[0],B[0]):
+    ans = abs(L[0] - B[0]) + abs(L[1] - B[1]) + 1
+else:
+    ans = abs(L[0] - B[0]) + abs(L[1] - B[1]) - 1
 print(ans)
