5f9aca9d974d9aa0c91b91295640b77fab06384e
윤성훈 <79916054+seonghun2@users.noreply.github.com>
2024-01-02 23:35:15 +0900

    Title: 좌표평면 위의 균형, Time: 118ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240102/좌표평면 위의 균형/balance-on-coordinate-plane.py
+++ 240102/좌표평면 위의 균형/balance-on-coordinate-plane.py
@@ -7,22 +7,22 @@ ans = sys.maxsize
 for _ in range(N):
     arr.append(list(map(int, input().split())))
 
-# for x in range(2, R, 2):
-#     for y in range(2, R, 2):
-#         a = 0
-#         b = 0
-#         c = 0
-#         d = 0
-#         for point in arr:
-#             if point[0] > x and point[1] > y:
-#                 a += 1
-#             elif point[0] > x and point[1] < y:
-#                 b += 1
-#             elif point[0] < x and point[1] > y:
-#                 c += 1
-#             elif point[0] < x and point[1] < y:
-#                 d += 1
+for x in range(2, R, 2):
+    for y in range(2, R, 2):
+        a = 0
+        b = 0
+        c = 0
+        d = 0
+        for point in arr:
+            if point[0] > x and point[1] > y:
+                a += 1
+            elif point[0] > x and point[1] < y:
+                b += 1
+            elif point[0] < x and point[1] > y:
+                c += 1
+            elif point[0] < x and point[1] < y:
+                d += 1
 
-#         ans = min(ans, max(a,b,c,d))
+    ans = min(ans, max(a,b,c,d))
 
 print(N//4+1)
