937c6e96adc8f0bc20b1f642d1b479aef39505cc
윤성훈 <79916054+seonghun2@users.noreply.github.com>
2024-01-04 21:47:57 +0900

    Title: 좌표평면 위의 균형, Time: 193ms, Memory: 26MB, Status: Passed - CodeTree

--- 240104/좌표평면 위의 균형/balance-on-coordinate-plane.py
+++ 240104/좌표평면 위의 균형/balance-on-coordinate-plane.py
@@ -2,37 +2,26 @@ import sys
 
 N, R = map(int, input().split())
 arr = []
-for _ in range(N):
-    arr.append(list(map(int, input().split())))
-xSpaces = []
-ySpaces = []
-xsortedArr = sorted(arr, key = lambda x : x[0])
-ysortedArr = sorted(arr, key = lambda x : x[0])
-
-for i in range(xsortedArr[0][0], xsortedArr[-1][0]):
-    if i % 2 == 0:
-        xSpaces.append(i)
-
-for i in range(ysortedArr[0][0], ysortedArr[-1][0]):
-    if i % 2 == 0:
-        ySpaces.append(i)
-
 ans = sys.maxsize
 
-for x in xSpaces:
-    for y in ySpaces:
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
+
+
+for i in range(N):
+    for j in range(N):
         a = 0
         b = 0
         c = 0
         d = 0
         for point in arr:
-            if point[0] > x and point[1] > y:
+            if point[0] > arr[i][0]+1 and point[1] > arr[j][1]+1:
                 a += 1
-            elif point[0] > x and point[1] < y:
+            elif point[0] > arr[i][0]+1 and point[1] < arr[j][1]+1:
                 b += 1
-            elif point[0] < x and point[1] > y:
+            elif point[0] < arr[i][0]+1 and point[1] > arr[j][1]+1:
                 c += 1
-            elif point[0] < x and point[1] < y:
+            elif point[0] < arr[i][0]+1 and point[1] < arr[j][1]+1:
                 d += 1
 
         ans = min(ans, max(a,b,c,d))
