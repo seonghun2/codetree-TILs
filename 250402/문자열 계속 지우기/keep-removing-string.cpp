4336ded77d25a9d58037737b74c79441c7db2a24
윤성훈 <79916054+seonghun2@users.noreply.github.com>
2025-04-02 21:00:11 +0900

    Title: 문자열 계속 지우기, Time: 8ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250402/문자열 계속 지우기/keep-removing-string.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+string A, B;
+
+int main() {
+    cin >> A;
+    cin >> B;
+    string ans = A;
+    
+    while (true) {
+        bool find = false;
+        for (int i = 0; i < ans.length() - B.length() + 1; i++) {
+            if (ans.substr(i, B.length()) == B) {
+                ans = ans.erase(i, B.length());
+                find = true; 
+            }
+        }
+        if (find == false) break;
+    }
+
+    cout << ans;
+    return 0;
+}
